3c9af8ae5a72ffec4a218fbe7d33e3364d71086b
Burak Yucel <burakyucelm@gmail.com>
2021-02-19 12:12:00 -0500

    Removed chat list from the tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ChatListComponent } from './chat-list/chat-list.component';
 import { ChatComponent } from './chat/chat.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -10,7 +9,6 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'chat', component: ChatComponent },
   { path: 'chat/:chatId', component: ChatComponent },
-  { path: 'chat-list', component: ChatListComponent },
 ];
 
 @NgModule({
