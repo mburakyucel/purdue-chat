77469a4eed5587dc1269c417b4e7609823b0a337
djarufes <djarufes@purdue.edu>
2021-02-28 11:26:03 -0500

    Working class search, can be made more efficient

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 
 import { Class } from '../../assets/class';
 import { SubscriptionService } from '../services/subscription.service';
@@ -11,17 +11,37 @@ import { SubscriptionService } from '../services/subscription.service';
 export class ClassesComponent implements OnInit {
   classes: Class[];
   selectedClass: Class;
+  searchText = '';
+  classSearch: Class[];
 
   constructor(private subService: SubscriptionService) {}
 
   ngOnInit() {
     this.subService
       .getClasses()
-      .subscribe((classes) => (this.classes = classes));
+      .subscribe(classes => {this.classes = classes, 
+        this.classSearch = this.classes});
   }
 
   onSelect(myclass: Class): void {
     this.selectedClass = myclass;
     this.subService.addSubscription(myclass.id);
+  }
+
+  classFilter(): void {
+    //Clear serchable class list
+    this.classSearch = [];
+
+    //Only compare the class name and number
+    let name: string;
+    for(let item of this.classes) 
+    {
+      name = item.subject + " " + item.course;
+      if(name.toLocaleLowerCase().includes(this.searchText) ||
+         name.includes(this.searchText))
+      {
+        this.classSearch.push(item);
+      }
+    }
   }
 }
