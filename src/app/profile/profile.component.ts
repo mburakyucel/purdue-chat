be972db2da58415e826a4e8495e8906e21419bb5
Ryan Harmon <harmonr@purdue.edu>
2021-03-02 00:14:44 -0500

    added incorrect password popup

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -69,6 +69,9 @@ export class ProfileComponent implements OnInit {
       }, 
       (error) => {
         console.log(error);
+        this._snackBar.open('Incorrect Password', 'Close', {
+          duration: 5000,
+        });
       });
     }
   }
