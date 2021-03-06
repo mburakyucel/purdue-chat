5d9fd04bcc7d215bc3485709d7513beb3471d9b8
root <root@MSI.localdomain>
2021-03-06 09:48:27 -0500

    Added join button inside dialog box

--- src/app/chat-info/chat-info.component.ts
+++ src/app/chat-info/chat-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 
 import { Class } from '../../assets/class';
 import { SubscriptionService } from '../services/subscription.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-chat-info',
@@ -9,25 +10,45 @@ import { SubscriptionService } from '../services/subscription.service';
   styleUrls: ['./chat-info.component.css']
 })
 export class ChatInfoComponent implements OnInit {
-  selectedClass: string = '';
+  selectedClassId: string = '';
+  selectedClassInfo: Class;
   allClasses: Class[] = [];
 
   constructor(
-    private subService: SubscriptionService
+    private subService: SubscriptionService,
+    private _snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
-    this.subService.currentSelectedClass.subscribe(Class => this.selectedClass = Class)
+    this.subService.currentSelectedClass.subscribe(Class => this.selectedClassId = Class)
     this.subService.getClasses().subscribe((classes) => this.allClasses = classes);
   }
 
   //Function that return the description of the selected class
   findClassInfo(): string {
     for(let item of this.allClasses) {
-      if (item.id == this.selectedClass) {
+      if (item.id == this.selectedClassId) {
+        this.selectedClassInfo = item;
         return item.description
       }
     }
     return ''
+  }
+
+  onJoinSelectInDialog(): void {
+    this.subService
+      .addSubscription(this.selectedClassId)
+      .then(() => {
+        this._snackBar.open(
+          `Subscribed to ${this.selectedClassInfo.subject} ${this.selectedClassInfo.course}`,
+          'Close',
+          {
+            duration: 2000,
+          }
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
