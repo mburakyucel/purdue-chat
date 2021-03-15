5666f59a8236da4a8e975a74a8082ed47c957a49
djarufes <djarufes@purdue.edu>
2021-03-15 12:27:28 -0400

    Login form Input validation following documentation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -12,8 +12,14 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent implements OnInit {
   loading = false;
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('',[
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(6)
+    ]),
   });
 
   constructor(
@@ -24,11 +30,11 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  async login() {
-    if(this.email && this.password) { 
+  async login() { 
+    if(!this.email.invalid && !this.password.invalid ) {
       this.loading = true;
       (
-        await this.authService.login(this.email, this.password)
+        await this.authService.login(this.email.value, this.password.value)
       ).subscribe(
         () => {
           this._snackBar.open('Login successful', 'Close', {
@@ -61,25 +67,10 @@ export class LoginComponent implements OnInit {
   }
 
   get email() {
-    var mailformat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (this.loginForm.get('email').value.match(mailformat)) {
-      return this.loginForm.get('email').value
-    }
-    else {
-      this._snackBar.open('The email address is badly formatted', 'Close', {
-        duration: 3000,
-      });
-    }
+    return this.loginForm.get('email');
   }
 
   get password() {
-    if (this.loginForm.get('password').value.length >= 6) {
-      return this.loginForm.get('password').value
-    }
-    else {
-      this._snackBar.open('The password is invalid', 'Close', {
-        duration: 3000,
-      });
-    }
+    return this.loginForm.get('password');
   }
 }
